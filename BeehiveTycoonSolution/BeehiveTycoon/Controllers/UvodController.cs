f473586baf6d6369d2c5d71cb7c10581fd21ad24
Amler Pavel <amler@spstrutnov.cz>
2022-01-29 21:01:55 +0100

    import verze 1.0

---
+++ BeehiveTycoonSolution/BeehiveTycoon/Controllers/UvodController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BeehiveTycoon.Models;
+
+namespace BeehiveTycoon.Controllers
+{
+    public class UvodController : UlController
+    {
+        public IActionResult Hrat()
+        {
+            return View();
+        }
+
+        public IActionResult Chyba()
+        {
+            return View();
+        }
+
+        public IActionResult Prohra(string tlacitko)
+        {
+            Hra hra = NacistHru();
+
+            if (hra.Ul.Vcelstvo > 0)
+                return Redirect(Url.Action("Plastev", "Ul"));
+
+            if (tlacitko == "znova")
+            {
+                hra = null;
+                UlozitHru(hra);
+                return Redirect(Url.Action("Plastev", "Ul"));
+            }
+
+            return View();
+        }
+
+        public IActionResult Vyhra(string tlacitko)
+        {
+            Hra hra = NacistHru();
+            if (hra.Vyhra == false)
+                return Redirect(Url.Action("Plastev", "Ul"));
+            if(tlacitko == "znova")
+            {
+                hra = null;
+                UlozitHru(hra);
+                return Redirect(Url.Action("Plastev", "Ul"));
+            }
+            else if(tlacitko == "pokracovat")
+            {
+                hra.Vyhra = false;
+                UlozitHru(hra);
+                return Redirect(Url.Action("Plastev", "Ul"));
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ BeehiveTycoonSolution/BeehiveTycoon/Models/Hra.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeehiveTycoon.Models
+{
+    public class Hra
+    {
+        public Ul Ul { get; set; }
+        public Nepritel Nepritel { get; set; }
+        public List<GeneraceVcelstva> GeneraceVcelstev { get; set; }
+        public List<Plastev> Plastve { get; set; }
+        public List<Ukol> SeznamUkolu { get; set; }
+        public bool Vyhra { get; set; }
+    }
+
+    public struct Ul
+    {
+        public int Vcelstvo { get; set; }
+        public int Med { get; set; }
+        public string Mesic { get; set; }
+    }
+
+    public struct GeneraceVcelstva
+    {
+        public int Pocet { get; set; }
+        public int Vek { get; set; }
+    }
+
+    public struct Ukol
+    {
+        public int IdUkolu { get; set; }
+        public string Nazev { get; set; }
+        public int PocetVcel { get; set; }
+        public int PocetVajicek { get; set; }
+        public int PocetMedu { get; set; }
+        public int PocetPlastvi { get; set; }
+        public int Platnost { get; set; }
+    }
+
+    public struct Pozadavek
+    {
+        public string Jmeno { get; set; }
+        public int Pocet { get; set; }
+    }
+
+    public struct Plastev
+    {
+        public int Med { get; set; }
+        public int Vajicka { get; set; }
+    }
+
+    public struct Nepritel
+    {
+        public int IdNepritele { get; set; }
+        public string JmenoNepritele { get; set; }
+        public int PocetNepratel { get; set; }
+        public int HPNepritele { get; set; }
+        public int DalsiUtok { get; set; }
+    }
+}
