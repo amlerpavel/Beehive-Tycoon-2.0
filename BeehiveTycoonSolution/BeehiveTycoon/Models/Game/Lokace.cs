dbd7d70c946475e463f6003f6a1cc4721263c868
Amler Pavel <amler@spstrutnov.cz>
2022-04-03 22:33:12 +0200

    Poznámky o lokacích

--- BeehiveTycoonSolution/BeehiveTycoon/Models/Game/Lokace.cs
+++ BeehiveTycoonSolution/BeehiveTycoon/Models/Game/Lokace.cs
@@ -15,5 +15,28 @@ namespace BeehiveTycoon.Models.Game
             Nazev = nazev;
             Id = id;
         }
+
+        /*
+        lokace:
+            Les
+                -hodně nepratel
+                -hodně medu, do května
+
+            Louka
+                -vice nepratel
+                -hodně medu, od května
+
+            Město
+                -nekolik vcel zemre
+                -malo medu
+                -mene nepratel
+
+            Pole
+                -nekolik vcel zemre
+                -hodne pylu v urcitou dobu, jinak malo
+
+            Sousedova zahrada
+                -neutralni lokace
+         */
     }
 }
