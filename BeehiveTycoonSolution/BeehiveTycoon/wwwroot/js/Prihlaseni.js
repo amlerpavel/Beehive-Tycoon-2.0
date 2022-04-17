7763b7faf4341d6f48d27b7553ade95e70ed439d
Novotný Viktor <novotnyv@spstrutnov.cz>
2022-04-17 13:50:23 +0200

    Uživatel se může přihlásit

--- BeehiveTycoonSolution/BeehiveTycoon/wwwroot/js/Prihlaseni.js
+++ BeehiveTycoonSolution/BeehiveTycoon/wwwroot/js/Prihlaseni.js
@@ -1,1 +1,29 @@
-﻿
+﻿$(document).ready(function () {
+    Prihlasit();
+});
+
+function ZiskatData() {
+    let dataPrihlaseni = { Jmeno: "", Heslo: "" };
+
+    dataPrihlaseni.Jmeno = $('input[name="jmeno"]').val();
+    dataPrihlaseni.Heslo = $('input[name="heslo"]').val();
+
+    return dataPrihlaseni;
+}
+
+function Prihlasit() {
+    $(document).on("click", "#prihlasit", function () {
+        fetch('/Uzivatel/Prihlasit', {
+            method: 'POST',
+            body: JSON.stringify(ZiskatData()),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(odpoved => odpoved.json())
+        .then(data => {
+            console.log(data);
+        });
+    });
+}
+
